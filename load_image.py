1324a4f1ebf389eb28c2fbc22592fcdf2562b131
De <dxvo@uh.edu>
2018-11-13 01:02:43 -0600

    update

--- load_image.py
+++ load_image.py
@@ -9,13 +9,16 @@ from PIL import Image, ImageTk
 class load_image:
     def __init__(self,master):
         self.master = master
-        self.c_size = (700,500)
+        self.c_size = (1000,500)
         self.setup_gui(self.c_size)
         self.img = None
 
     def setup_gui(self,size):
         Label(self.master, text = "Image Restoration", pady = 5, bg = "white", font = ("",30)).pack()
-        self.canvas = Canvas(self.master, height=size[1],width= size[0],bd=10,bg='black',relief = "ridge")
+        self.canvas = Canvas(self.master, height=size[1]/2,width= size[0]/2,bd=10,bg='black',relief = "ridge")
+        self.canvas2 = Canvas(self.master, height=size[1]/2, width=size[0]/2, bd=10, bg='black', relief="ridge")
+
+        self.canvas2.pack()
         self.canvas.pack()
         txt = ""
         self.wt = self.canvas.create_text(size[0]/2-270,size[1]/2,text =txt, font =("",30),fill = "white")
@@ -30,7 +33,9 @@ class load_image:
         try:
             File = fd.askopenfilename()#return the path
             self.pilImage = Image.open(File).convert('LA') #display image from path and covert to LA
-            resize = self.pilImage.resize((700,500),Image.ANTIALIAS)#scale it to window size
+            #img.save('greyscale.png') //this is to save the image
+
+            #resize = self.pilImage.resize((700,500),Image.ANTIALIAS)#scale it to window size
             self.img = ImageTk.PhotoImage(self.pilImage)
             self.canvas.delete(ALL)
             self.canvas.create_image(self.c_size[0]/2+10,self.c_size[1]/2+10,anchor = CENTER, image = self.img)
@@ -39,8 +44,8 @@ class load_image:
             ms.showerror("Error", "File is noto supported")
 
 root  = Tk()
-root.title("Image Viewer")
-root.resizable(0,0)
+root.title("FIRST_ROW_DIP")
+#root.resizable(0,0)
 load_image(root)
 root.mainloop()
 
