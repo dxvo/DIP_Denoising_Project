d580cd66ebcf21c8bc1eea149c2b40f4ba4f7563
De <dxvo@uh.edu>
2018-11-17 01:07:54 -0600

    update main

--- Gamma_Noise_Filter.py
+++ Gamma_Noise_Filter.py
@@ -21,33 +21,38 @@ class Gamma(tk.Tk):
         noise_image_label = Label(self, image=self.image_noise)
         noise_image_label.pack(side=LEFT)
 
-        self.arithmetic_button = Button(self, text=" Arithmetic Mean Filter", fg="blue", font=("", 20),command = self.print)
-        self.arithmetic_button.pack(side=TOP)
-
-        self.geometric_button = Button(self, text=" Geometric Mean Filter", fg="blue", font=("", 20),command=self.print)
-        self.geometric_button.pack(side=TOP)
-
-        self.harmonic_button = Button(self, text=" Harmonic Mean Filter", fg="blue", font=("", 20),command=self.print)
-        self.harmonic_button.pack(side=TOP)
-
-        self.contra_harmonic_button = Button(self, text=" Contraharmonic Mean Filter", fg="blue", font=("", 20),command=self.print)
-        self.contra_harmonic_button.pack(side=TOP)
-
         # Order statistic
-        self.median_button = Button(self, text="Median Filter", fg="blue", font=("", 20),command=self.print)
-        self.median_button.pack(side=TOP)
+        self.median_button = Button(self, text="Median Filter", fg="blue", font=("", 20), command=self.print)
+        self.median_button.pack(side=TOP, fill=BOTH)
 
         self.min_button = Button(self, text="Min Filter", fg="blue", font=("", 20), command=self.print)
-        self.min_button.pack(side=TOP)
+        self.min_button.pack(side=TOP, fill=BOTH)
 
         self.max_button = Button(self, text="Max Filter", fg="blue", font=("", 20), command=self.print)
         self.max_button.pack(side=TOP, fill=BOTH)
 
-        self.trimmed_button = Button(self, text="Alpha Trimmed Mean Filter", fg="blue", font=("", 20), command=self.print)
-        self.trimmed_button.pack(side=TOP)
-
         self.adaptive_button = Button(self, text="Adaptive Filter", fg="blue", font=("", 20), command=self.print)
-        self.adaptive_button.pack(side=TOP)
+        self.adaptive_button.pack(side=TOP, fill=BOTH)
+
+        # mean
+        self.arithmetic_button = Button(self, text=" Arithmetic Mean Filter", fg="blue", font=("", 20),
+                                        command=self.print)
+        self.arithmetic_button.pack(side=TOP, fill=BOTH)
+
+        self.geometric_button = Button(self, text=" Geometric Mean Filter", fg="blue", font=("", 20),
+                                       command=self.print)
+        self.geometric_button.pack(side=TOP, fill=BOTH)
+
+        self.harmonic_button = Button(self, text=" Harmonic Mean Filter", fg="blue", font=("", 20), command=self.print)
+        self.harmonic_button.pack(side=TOP, fill=BOTH)
+
+        self.contra_harmonic_button = Button(self, text=" Contraharmonic Mean Filter", fg="blue", font=("", 20),
+                                             command=self.print)
+        self.contra_harmonic_button.pack(side=TOP, fill=BOTH)
+
+        self.trimmed_button = Button(self, text="Alpha Trimmed Mean Filter", fg="blue", font=("", 20),
+                                     command=self.print)
+        self.trimmed_button.pack(side=TOP, fill=BOTH)
 
         self.restart_button = Button(self, text="Restart Program", fg="RED", font=("", 20), highlightbackground='Yellow', command=self.restart)
         self.restart_button.pack(side=BOTTOM)
--- Salt_Noise_Filter.py
+++ Salt_Noise_Filter.py
@@ -14,40 +14,45 @@ class Salt(tk.Tk):
         self.title("SALT NOISE FILTERING")
         self.configure(background='black')
 
-        gaussian_image_path = "Noise/Salt_Noise.png"
-        self.PIL_image = Image.open(gaussian_image_path)
-        self.image_add_gaussian_noise = ImageTk.PhotoImage(self.PIL_image)
+        image_path = "Noise/Salt_Noise.png"
+        self.PIL_image = Image.open(image_path)
+        self.image_noise = ImageTk.PhotoImage(self.PIL_image)
 
-        gaussian_noise_image_label = Label(self, image=self.image_add_gaussian_noise)
-        gaussian_noise_image_label.pack(side=LEFT)
-
-        self.arithmetic_button = Button(self, text=" Arithmetic Mean Filter", fg="blue", font=("", 20),command = self.print)
-        self.arithmetic_button.pack(side=TOP)
-
-        self.geometric_button = Button(self, text=" Geometric Mean Filter", fg="blue", font=("", 20),command=self.print)
-        self.geometric_button.pack(side=TOP)
-
-        self.harmonic_button = Button(self, text=" Harmonic Mean Filter", fg="blue", font=("", 20),command=self.print)
-        self.harmonic_button.pack(side=TOP)
-
-        self.contra_harmonic_button = Button(self, text=" Contraharmonic Mean Filter", fg="blue", font=("", 20),command=self.print)
-        self.contra_harmonic_button.pack(side=TOP)
+        noise_image_label = Label(self, image=self.image_noise)
+        noise_image_label.pack(side=LEFT)
 
         # Order statistic
-        self.median_button = Button(self, text="Median Filter", fg="blue", font=("", 20),command=self.print)
-        self.median_button.pack(side=TOP)
+        self.median_button = Button(self, text="Median Filter", fg="blue", font=("", 20), command=self.print)
+        self.median_button.pack(side=TOP, fill=BOTH)
 
         self.min_button = Button(self, text="Min Filter", fg="blue", font=("", 20), command=self.print)
-        self.min_button.pack(side=TOP)
+        self.min_button.pack(side=TOP, fill=BOTH)
 
         self.max_button = Button(self, text="Max Filter", fg="blue", font=("", 20), command=self.print)
         self.max_button.pack(side=TOP, fill=BOTH)
 
-        self.trimmed_button = Button(self, text="Alpha Trimmed Mean Filter", fg="blue", font=("", 20), command=self.print)
-        self.trimmed_button.pack(side=TOP)
-
         self.adaptive_button = Button(self, text="Adaptive Filter", fg="blue", font=("", 20), command=self.print)
-        self.adaptive_button.pack(side=TOP)
+        self.adaptive_button.pack(side=TOP, fill=BOTH)
+
+        # mean
+        self.arithmetic_button = Button(self, text=" Arithmetic Mean Filter", fg="blue", font=("", 20),
+                                        command=self.print)
+        self.arithmetic_button.pack(side=TOP, fill=BOTH)
+
+        self.geometric_button = Button(self, text=" Geometric Mean Filter", fg="blue", font=("", 20),
+                                       command=self.print)
+        self.geometric_button.pack(side=TOP, fill=BOTH)
+
+        self.harmonic_button = Button(self, text=" Harmonic Mean Filter", fg="blue", font=("", 20), command=self.print)
+        self.harmonic_button.pack(side=TOP, fill=BOTH)
+
+        self.contra_harmonic_button = Button(self, text=" Contraharmonic Mean Filter", fg="blue", font=("", 20),
+                                             command=self.print)
+        self.contra_harmonic_button.pack(side=TOP, fill=BOTH)
+
+        self.trimmed_button = Button(self, text="Alpha Trimmed Mean Filter", fg="blue", font=("", 20),
+                                     command=self.print)
+        self.trimmed_button.pack(side=TOP, fill=BOTH)
 
         self.restart_button = Button(self, text="Restart Program", fg="RED", font=("", 20), highlightbackground='Yellow', command=self.restart)
         self.restart_button.pack(side=BOTTOM)
